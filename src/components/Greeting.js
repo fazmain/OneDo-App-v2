a6569380f038bec55423fefc2b89b64af21c0d43
fazmain <faiaz.azmain99@gmail.com>
2023-12-13 13:18:46 +0600

    Change style

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -35,7 +35,7 @@ const Greeting = ({ username }) => {
   };
 
   return (
-    <Box p = {3} borderRadius={"lg"} bg="blue.900">
+    <Box p = {3} borderRadius={"lg"} borderWidth={1}>
     <VStack
       align="flex-start"
       width="100%"
